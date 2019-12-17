f77727577ce8e6edc24ce9ae577d3de650336b05
v_gaochaofeng01 <v_gaochaofeng01@baidu.com>
2019-12-17 10:27:39 +0800

    js-extends

---
+++ vue/myVue.js
@@ -0,0 +1,30 @@
+
+
+ 
+// class myVue { 
+//     constructor(el,data) {
+        
+//         // this.watcher()
+//     }
+
+//     watcher() {
+//         console.log('==watcher is read==')
+//     }
+// }
+
+
+// let v = new myVue()
+
+
+// let str = '哈哈是大法师asd123大师傅'
+
+// str.split('').map(k => {
+//     // console.log(k)
+//     console.log(/[a-z0-9]/.test(k))
+// })
+
+let arr =[1,2,3,4,5];
+
+
+console.log(arr.slice(0, 4));
+console.log(arr.splice(0,4))
